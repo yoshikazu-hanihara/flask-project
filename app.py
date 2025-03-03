4d070e0a7cc7d094c7d7d13289275d1df7316c95
yoshikazu-hanihara <122194402+yoshikazu-hanihara@users.noreply.github.com>
2025-03-03 11:49:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,15 @@ from flask import Flask
 from flask_mail import Mail
 import os
 import json
-from passlib.hash import bcrypt_sha256
-from datetime import datetime
-
-# DB接続用 (例: PyMySQL)
 from db import get_connection
 
-# 既存の他のBlueprint (auth, estimate) の読み込み
-from auth import auth as auth_blueprint
-from estimate import estimate_blueprint
-
-# 今回新たに分割した dashboard.py (blueprints配下) の読み込み
+# blueprint 読み込み
 from blueprints.dashboard import dashboard as dashboard_blueprint
 
-######################################
-# Flask基本設定
-######################################
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # セッション用キー(例)
+app.secret_key = 'your_secret_key'
 
-# Flask-Mail の設定 (例: Gmail)
+# (例) Flask-Mail の設定
 app.config['MAIL_SERVER']   = 'smtp.gmail.com'
 app.config['MAIL_PORT']     = 587
 app.config['MAIL_USE_TLS']  = True
@@ -30,16 +19,8 @@ app.config['MAIL_PASSWORD'] = 'yspr vktd yrmc wntn'
 app.config['MAIL_DEFAULT_SENDER'] = 'nworks12345@gmail.com'
 mail = Mail(app)
 
-# Blueprint の登録
-app.register_blueprint(auth_blueprint, url_prefix='')
-app.register_blueprint(estimate_blueprint, url_prefix='')
-
-# 新しく分割したダッシュボード用Blueprint
+# Blueprint登録: blueprint名='dashboard'、URLプリフィックスは空にしている
 app.register_blueprint(dashboard_blueprint, url_prefix='')
 
-
-######################################
-# メイン起動
-######################################
 if __name__ == '__main__':
     app.run(debug=True)
