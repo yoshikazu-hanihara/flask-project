4723c38b179bda0533dfbc43e1c163c68ecea1d2
yoshikazu-hanihara <122194402+yoshikazu-hanihara@users.noreply.github.com>
2025-04-04 17:24:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mail import Mail
-import os
-import json
-from db import get_connection
 from blueprints.dashboard import dashboard_bp
+from blueprints.auth import auth_bp
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# (例) Flask-Mail の設定
-app.config['MAIL_SERVER']   = 'smtp.gmail.com'
-app.config['MAIL_PORT']     = 587
-app.config['MAIL_USE_TLS']  = True
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'nworks12345@gmail.com'
 app.config['MAIL_PASSWORD'] = 'yspr vktd yrmc wntn'
 app.config['MAIL_DEFAULT_SENDER'] = 'nworks12345@gmail.com'
 mail = Mail(app)
 
-# Blueprint登録: blueprint名='dashboard'、URLプリフィックス/dashboard
+# Blueprintを登録
+app.register_blueprint(auth_bp, url_prefix='')
 app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
+
+@app.route('/')
+def index():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
