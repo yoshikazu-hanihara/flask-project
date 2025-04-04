fac9bb57e0f804b5bca781d276c7e032a86b2765
yoshikazu-hanihara <122194402+yoshikazu-hanihara@users.noreply.github.com>
2025-04-04 17:18:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app.config['MAIL_PASSWORD'] = 'yspr vktd yrmc wntn'
 app.config['MAIL_DEFAULT_SENDER'] = 'nworks12345@gmail.com'
 mail = Mail(app)
 
-# Blueprint登録: blueprint名='dashboard'、URLプリフィックスは空にしている
-app.register_blueprint(dashboard_bp, url_prefix='')
+# Blueprint登録: blueprint名='dashboard'、URLプリフィックス/dashboard
+app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 
 if __name__ == '__main__':
     app.run(debug=True)
