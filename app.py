20832aecd59f5bf439b303bb0fc4370e298a8d65
yoshikazu-hanihara <122194402+yoshikazu-hanihara@users.noreply.github.com>
2025-04-20 11:17:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from blueprints.dashboard import dashboard_bp
 from blueprints.auth import auth
 from estimate import estimate_blueprint
 from blueprints.export import export_bp
-
+from blueprints.preset import preset_bp
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -21,6 +21,7 @@ app.register_blueprint(auth, url_prefix='')
 app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 app.register_blueprint(estimate_blueprint, url_prefix='/estimate')
 app.register_blueprint(export_bp)
+app.register_blueprint(preset_bp)
 
 @app.route('/')
 def index():
